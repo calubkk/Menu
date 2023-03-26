856ce2874170b1d996bdc2e202c67fc23e9a67ba
calubkk <827490054@qq.com>
2023-03-26 15:24:44 +0800

    Menu Version2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/calubkk/Menu
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+ 
+import (
+	"fmt"
+	"os"
+ 
+	"github.com/calubkk/Menu/linklist"
+)
+ 
+const (
+	CMD_MAX_LEN = 128
+	DESC_LEN    = 1024
+	CMD_NUM     = 10
+)
+ 
+func main() {
+	for {
+		cmd := make([]byte, CMD_MAX_LEN)
+		fmt.Print(">>> Input a command: ")
+		fmt.Scanln(&cmd)
+		p := linklist.FindCmd(head, string(cmd))
+		if p == nil {
+			fmt.Println("This is a wrong cmd!")
+			continue
+		}
+		fmt.Printf("%s - %s\n", p.Cmd, p.Desc)
+		if p.Cmd == "help" {
+			help()
+		}
+		if p.Handler != nil {
+			p.Handler()
+		}
+	}
+}
+ 
+var head *linklist.DataNode = &linklist.DataNode{
+	Cmd:     "help",
+	Desc:    "this is help command",
+	Handler: nil,
+	Next: &linklist.DataNode{
+		Cmd:     "version",
+		Desc:    "menu program v1.0",
+		Handler: nil,
+		Next: &linklist.DataNode{
+			Cmd:     "quit",
+			Desc:    "exit the program",
+			Handler: quit,
+			Next:    nil,
+		},
+	},
+}
+ 
+var help = func() int {
+	linklist.ShowAllCmd(head)
+	return 0
+}
+ 
+var quit = func() int {
+	fmt.Println("Bye.")
+	os.Exit(0)
+	return 0
+}
